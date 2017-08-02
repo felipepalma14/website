0d097ef087d59b23040f37d428bc70b844c9c0fb
felipe <felipepalma14@gmail.com>
2017-08-02 18:22:23 -0400

    cadastrado de produto, lista com table modelos de carro

--- src/scripts/controllers/DashboardControle.js
+++ src/scripts/controllers/DashboardControle.js
@@ -16,36 +16,67 @@
 			$scope.modelos =  [];
 			$scope.anos    =  [];
 
+			$scope.carro = {};
+
+
 			$scope.produtos  = APIService.getProdutos();
 			$scope.categorias = APIService.getCategorias();
 //APIService.getModelos();
 
+			(function(){
+				FIPEService.getMarcasFIPE()
+				.then(function(resposta) {
+					//console.log(resposta.data);
+                    $scope.marcas = resposta.data;
+                    $scope.carro.marca = resposta.data[0];
+                   	
+                   	$scope.encontrarModelos($scope.carro.marca);
+                    //$scope.encontrarAnos($scope.carro.marca,$scope.carro.modelo);
+                });
+
+			})();
 			$scope.single = function(image) {
                     console.log(image);
              };
 
-			FIPEService.getMarcasFIPE()
-				.then(function(resposta) {
-					//console.log(resposta.data);
-                    $scope.marcas = resposta.data;
-                });
-
+            
+			
 			$scope.encontrarModelos = function(marca){
 				FIPEService.getModelosFIPE(marca)
 				.then(function(resposta) {
 					//console.log(resposta.data.modelos);
                     $scope.modelos = resposta.data.modelos;
-                    $scope.carro.modelo = $scope.modelos[0];
+                    $scope.carro.modelo = resposta.data.modelos[0];//$scope.modelos[0];
+
+                    
                 });
 			};
 			
 			$scope.encontrarAnos = function(marca,modelo){
+
 				FIPEService.getAnosFIPE(marca,modelo)
 				.then(function(resposta) {
 					console.log(resposta.data);
                     $scope.anos = resposta.data;
+                    $scope.carro.ano = $scope.anos[0];
                 });
+                
 			};
 
+
+			/*
+				Criando lista de carros 
+			*/
+
+			$scope.carros = [];
+
+			$scope.addMarcaModelo = function(dados){
+				var novoCarro = novoCarro || {};
+				novoCarro.marca = dados.marca;
+				novoCarro.modelo = dados.modelo;
+				novoCarro.ano = dados.ano; 
+				console.log(novoCarro);
+				$scope.carros.push(novoCarro);
+			};
 		}
 }) ();
