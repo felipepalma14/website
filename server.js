3509849b71a93bef91e1e90f2e1b6c2ac25f9d25
felipe <felipepalma14@gmail.com>
2017-09-17 22:08:46 -0400

    atualizacao de arquivos de configuracao package.json

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// app-server.js
+var express = require('express');
+var bodyParser = require('body-parser');
+var stripe = require("stripe")(process.env.STRIPE_SECRET);
+var app = express();
+app.set('port', process.env.PORT || 3000)
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+var http = require('http').Server(app)
+// Route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+app.post('/charge', (req, res) => {
+  var token = req.body.stripeToken;
+  // Charge the user's card:
+  var charge = stripe.charges.create({
+    amount: req.body.amount,
+    currency: "usd",
+    description: req.body.amount.description,
+    metadata: req.body.order,
+    source: token,
+  }, function(err, charge) {
+      res.send(charge);
+  });
+});
+http.listen(app.get('port'), () => {
+  console.log(' App listening on ' + app.get('port'))
+})
+/*
 var express = require("express");
 
 var app = express();
@@ -19,3 +49,4 @@ app.get('/',function(req,res){
 app.listen(3000);
 
 console.log("Ta funcionando");
+*/
